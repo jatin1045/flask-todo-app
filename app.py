52c7dd6a4bfc5a71c18128d75cb74bed1d4a1049
Jatin <jatinjoshi1045@gmail.com>
2025-07-18 16:38:24 +0530

    Only Item ID field in form

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
-from flask import Flask, render_template, request, redirect, url_for
+# MongoDB setup
+from flask import Flask, render_template, request, redirect
+from pymongo import MongoClient
 
 app = Flask(__name__)
-tasks = []
 
-@app.route('/')
-def index():
-    return render_template('index.html', tasks=tasks)
+client = MongoClient("mongodb://localhost:27017/")
+db = client["todo_db"]
+collection = db["items"]
 
-@app.route('/add', methods=['POST'])
-def add():
-    task = request.form.get('task')
-    if task:
-        tasks.append(task)
-    return redirect(url_for('index'))
-
+# To-Do Page route
 @app.route("/todo")
 def todo():
     return render_template("todo.html")
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# Backend route to handle form submission
+@app.route("/submittodoitem", methods=["POST"])
+def submit_todo_item():
+    item_name = request.form.get("itemName")
+    item_desc = request.form.get("itemDescription")
+    
+    if item_name and item_desc:
+        collection.insert_one({"name": item_name, "description": item_desc})
+    
+    return redirect("/todo")
